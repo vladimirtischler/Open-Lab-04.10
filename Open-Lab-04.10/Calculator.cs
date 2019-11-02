0ddc5c70e603958f38a187078c0c283ec139e97e
Erik Zemčík <zemcik.erik@gmail.com>
2019-11-02 22:17:19 +0100

    Created tests

---
+++ Open-Lab-04.10/Calculator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Open_Lab_04._10
+{
+    public class Calculator
+    {
+        public float Average(int[] nums)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Open-Lab-04.10/Tests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+
+namespace Open_Lab_04._10
+{
+    [TestFixture]
+    public class Tests
+    {
+
+        private Calculator calc;
+        private bool shouldStop;
+
+        private const float Tolerance = 0.02f;
+
+        private const int RandSeed = 410410410;
+        private const int RandNumsCountMin = 1;
+        private const int RandNumsCountMax = 100;
+        private const int RandTestCasesCount = 97;
+
+        [OneTimeSetUp]
+        public void Init()
+        {
+            calc = new Calculator();
+            shouldStop = false;
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            var outcome = TestContext.CurrentContext.Result.Outcome;
+
+            if (outcome == ResultState.Failure || outcome == ResultState.Error)
+                shouldStop = true;
+        }
+
+        [TestCase(new []{ 1, 0, 4, 5, 2, 4, 1, 2, 3, 3, 3 }, 2.54f)]
+        [TestCase(new []{ 2, 3, 2, 3 }, 2.5f)]
+        [TestCase(new []{ 3, 3, 3, 3, 3 }, 3f)]
+        public void AverageTest(int[] nums, float expected) =>
+            Assert.That(calc.Average(nums), Is.EqualTo(expected).Within(Tolerance));
+
+        [TestCaseSource(nameof(GetRandom))]
+        public void AverageTestRandom(int[] nums, float expected)
+        {
+            if (shouldStop)
+                Assert.Ignore("Previous test failed!");
+
+            AverageTest(nums, expected);
+        }
+
+        private static IEnumerable GetRandom()
+        {
+            var rand = new Random(RandSeed);
+
+            for (var i = 0; i < RandTestCasesCount; i++)
+            {
+                var nums = new int[rand.Next(RandNumsCountMin, RandNumsCountMax + 1)];
+
+                for (var j = 0; j < nums.Length; j++)
+                    nums[j] = rand.Next(int.MaxValue / nums.Length);
+
+                yield return new TestCaseData(nums, nums.Sum() / (float) nums.Length);
+            }
+        }
+
+    }
+}
